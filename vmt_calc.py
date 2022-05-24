f64cb18eee880291df472b4110ad341f36f841b6
ajsauter <anthony.j.sauter@gmail.com>
2022-05-24 14:37:16 -0600

    updated vmt_calc to incorp sys args

--- vmt_calc.py
+++ vmt_calc.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import random
 import xlsxwriter
 import numpy as np
@@ -11,42 +12,48 @@ from tract_generation import tract_generation
 # COMMON INPUTS
 # source = "Surf"
 # trat range: np.size(tot_tracts)]
-def vmt_calc(source, pt_num, tr1, tr2):    
-    # Set seed for stochastics to maintain repeatability across results
-    random.seed(26)
-    # Set Directories
-    [home_dir, data_dir, output_dir] = set_paths(source)
-    
-    # Gather DataFrames and Load List
-    [og_load_list, ACS_df, BTS_df, links_df, tract_by_county_df, geo_df] = data_gather(data_dir)
-    # Generate List of Tracts
-    tot_tracts = tract_generation(og_load_list, geo_df, tract_by_county_df)
-    
-    no_room_exists = 0
-    hh_mem_not_int = 0
-    supplanted_tracts = 0
-    single_veh_double_rate = 0
-    checks = [no_room_exists, hh_mem_not_int, supplanted_tracts, single_veh_double_rate]
-    
-    # geo_distance = []
-    g_dist_sum = []
-    
-    os.chdir(output_dir)
-    
-    for g in range(tr1, tr2):
-        # Function for tract distance calculation
-        [tot_tract_distance, checks] = tract_distance(g, tot_tracts, BTS_df, ACS_df, checks)
-        # geo_distance.append(tot_tract_distance)
-        g_dist_sum.append(sum(tot_tract_distance))
-        print("TRACTS COMPLETE: ", g)
-    
-    wb1 = xlsxwriter.Workbook('Tract_Distances_p' + pt_num + '.xlsx')
-    w1 = wb1.add_worksheet('Tract VMT')
-    w1.write(0, 0, 'Tract Names')
-    w1.write(0, 1, 'Total Distance')
-    w1.write_column(1, 0, tot_tracts)
-    w1.write_column(1, 1, g_dist_sum)
-    wb1.close()
+# def vmt_calc(source, pt_num, tr1, tr2):
+
+source = sys.argv[1]
+pt_num = sys.argv[2]
+tr1 = int(sys.argv[3])
+tr2 = int(sys.argv[4])
+
+# Set seed for stochastics to maintain repeatability across results
+random.seed(26)
+# Set Directories
+[home_dir, data_dir, output_dir] = set_paths(source)
+
+# Gather DataFrames and Load List
+[og_load_list, ACS_df, BTS_df, links_df, tract_by_county_df, geo_df] = data_gather(data_dir)
+# Generate List of Tracts
+tot_tracts = tract_generation(og_load_list, geo_df, tract_by_county_df)
+
+no_room_exists = 0
+hh_mem_not_int = 0
+supplanted_tracts = 0
+single_veh_double_rate = 0
+checks = [no_room_exists, hh_mem_not_int, supplanted_tracts, single_veh_double_rate]
+
+# geo_distance = []
+g_dist_sum = []
+
+os.chdir(output_dir)
+
+for g in range(tr1, tr2):
+    # Function for tract distance calculation
+    [tot_tract_distance, checks] = tract_distance(g, tot_tracts, BTS_df, ACS_df, checks)
+    # geo_distance.append(tot_tract_distance)
+    g_dist_sum.append(sum(tot_tract_distance))
+    print("TRACTS COMPLETE: ", g)
+
+wb1 = xlsxwriter.Workbook('Tract_Distances_p' + pt_num + '.xlsx')
+w1 = wb1.add_worksheet('Tract VMT')
+w1.write(0, 0, 'Tract Names')
+w1.write(0, 1, 'Total Distance')
+w1.write_column(1, 0, tot_tracts[tr1:tr2])
+w1.write_column(1, 1, g_dist_sum)
+wb1.close()
 
 """
 ADDRESS THIS WHEN CONSUMPTION IS INTRODUCED (SEPARATE FUNCTION)
